04b9f0a7f7ddd4653a5c87dbf5dea11b5040fdab
Adryan Eka Vandra <adryanekavandra@gmail.com>
2026-06-06 17:23:42 +0800

    fix: implement process kill and update workspace status

--- cmd/stop.go
+++ cmd/stop.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/adryanev/orkestra/pkg/process"
+	"github.com/adryanev/orkestra/pkg/workspace"
 	"github.com/spf13/cobra"
 )
 
@@ -21,9 +23,19 @@ var stopCmd = &cobra.Command{
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 			os.Exit(1)
 		}
+		// Update workspace status to inactive
+		ws, err := workspaceManager.GetWorkspace(stopWorkspace)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: could not get workspace status for %s: %v\n", stopWorkspace, err)
+		} else {
+			ws.Status = "inactive"
+			if err := workspaceManager.Save(); err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: failed to update workspace status for %s: %v\n", stopWorkspace, err)
+			}
+		}
 		fmt.Printf("Workspace %s stopped\n", stopWorkspace)
 	},
-}
+ }
 
 func init() {
 	stopCmd.Flags().StringVar(&stopWorkspace, "workspace", "", "Workspace ID")
